9456e89205927226184552dd781745b1c808bf6e
不朽的战士 <60593682+jiazhibai@users.noreply.github.com>
2025-02-22 12:44:04 +0800

    Update main.py

--- main.py
+++ main.py
@@ -10,24 +10,24 @@ from json_oper import store_json, load_json
 from sixyin import search, verify_key, get_download_link
 
 # playlist_id = '3222851321'  # QQ音乐歌单ID，通过分享获取
-playlist_id = '9118888178'  # QQ音乐歌单ID，通过分享获取
+playlist_id = '9440426249'  # QQ音乐歌单ID，通过分享获取
 unlock_key = 'B28D'  # 需通过flac.life官网免费获取解锁码
 cache_dir = './cache'
 music_dir = './music'
 start_at_index = 0
-paylist_raw_json_path = os.path.join(cache_dir, 'playlist.{0}.raw.json'.format(playlist_id))
-paylist_info_json_path = os.path.join(cache_dir, 'playlist.{0}.json'.format(playlist_id))
+paylist_raw_json_path = os.path。join(cache_dir, 'playlist.{0}.raw.json'。format(playlist_id))
+paylist_info_json_path = os.path。join(cache_dir, 'playlist.{0}.json'。format(playlist_id))
 
-if not os.path.exists(cache_dir):
+if not os.path。exists(cache_dir):
     os.makedirs(cache_dir)
 
-if not os.path.exists(music_dir):
+if not os.path。exists(music_dir):
     os.makedirs(music_dir)
 
 session = requests.Session()
-retries = Retry(total=3, backoff_factor=1)
-session.mount('http://', HTTPAdapter(max_retries=retries))
-session.mount('https://', HTTPAdapter(max_retries=retries))
+retries = 重试(total=3, backoff_factor=1)
+session.mount('http://'， HTTPAdapter(max_retries=retries))
+session.mount('https://'， HTTPAdapter(max_retries=retries))
 
 # 需通过flac.life官网免费获取解锁码
 stat = verify_key(unlock_key)
